4ee9edf6b1f4cf03b4a20825fd4da87e448c5d9a
JMartJonesy <jem4687@rit.edu>
2025-10-30 20:32:26 -0700

    Setup initial configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,40 @@
+{
+  pkgs,
+}:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./disko.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "j-stash";
+
+  # Enable mdadm for RAID
+  boot.swraid.enable = true;
+
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = "no";
+  };
+
+  users.users.jmartjonesy = {
+    isNormalUser = true;
+    description = "JMartJonesy Biznatch";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    initialPassword = "Password123";
+  };
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    git
+  ];
+
+  time.timeZone = "America/Los_Angeles";
+  system.stateVersion = "24.11";
+}
--- flake.nix
+++ flake.nix
@@ -8,22 +8,6 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    /*
-      sops-nix = {
-        url = "github:Mic92/sops-nix";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      kickstart-nixvim = {
-        url = "path:/home/jmartjonesy/Projects/kickstart.nixvim";
-      };
-      stylix = {
-        url = "github:danth/stylix";
-      };
-    */
   };
 
   outputs =
@@ -31,12 +15,6 @@
       self,
       nixpkgs,
       disko,
-      /*
-        sops-nix,
-        home-manager,
-        kickstart-nixvim,
-        stylix,
-      */
       ...
     }@inputs:
     {
@@ -47,19 +25,10 @@
             # Pass individual inputs by name to avoid recursion
             inherit
               disko
-              /*
-                lanzaboote
-                kickstart-nixvim
-                stylix
-              */
               ;
           };
           modules = [
-            ./hosts/framework16/configuration.nix
-            /*
-              sops-nix.nixosModules.sops
-              home-manager.nixosModules.home-manager
-            */
+            ./configuration.nix
           ];
         };
       };
